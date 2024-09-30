f130914a31196e8a5933de5985647bff31e87edf
denis.galin <denis.galin37@gmail.com>
2024-09-30 13:42:51 +0700

    basic logics of rating and controllers for players and stats

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class Player < ApplicationRecord
+  has_many :stats
+  def rating
+    case role
+    when 'forward'
+      stats.sum { |s| s.goals * 3 + s.assists * 2 + s.shots * 1 }
+    when 'midfielder'
+      stats.sum { |s| s.goals * 3 + s.assists * 2 + s.passes * 1 }
+    when 'defender'
+      stats.sum { |s| s.saves * 3 + s.passes * 2 }
+    when 'goalkeeper'
+      stats.sum { |s| s.saves * 5 + s.passes * 1 }
+    else
+      0
+    end
+  end
 end
